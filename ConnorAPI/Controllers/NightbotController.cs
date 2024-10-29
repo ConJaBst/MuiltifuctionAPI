68c57fabf6df9e13ddfa80d6bc0b8511853062e5
Connor <st10290665@vcconnect.edu.za>
2024-10-30 01:43:22 +0200

    initial commit

---
+++ ConnorAPI/Controllers/NightbotController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConnorAPI.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NightbotController : Controller
+    {
+        [HttpGet("{input}")]
+        public ActionResult GetResponse(int input)
+        {
+            if (input == 1)
+                return Ok("hi");
+            else if (input == 2)
+                return Ok("bye");
+            else
+                return BadRequest("Invalid input");
+        }
+    }
+}
