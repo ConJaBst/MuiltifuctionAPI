3d34337393e9f60cf6a7c94c475b08b9429cb172
Connor <st10290665@vcconnect.edu.za>
2024-11-02 23:52:59 +0200

    Made things prettier

--- ConnorAPI/obj/Debug/net8.0/ConnorAPI.AssemblyInfo.cs
+++ ConnorAPI/obj/Debug/net8.0/ConnorAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConnorAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+259710bb8564bd36c15542515a3a77fdd5022e7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77db55cab999530139ad9029b578825ae012fa48")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConnorAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConnorAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
